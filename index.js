bdb0637a3098252ebcd27b7dd551fa7c9fd9c497
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-09-28 01:10:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", 'Garfield']
+
+beforeEach(function() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+}
+)
+
+function destructivelyAppendCat() {
+     cats.push('Ralph')
+     return cats
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+}
+function appendCat() {
+    const newCat = [...cats.slice(), 'Broom']
+    return newCat
+}
+function prependCat() {
+    const anotherOne = ['Arnold', ...cats.slice()]
+    return anotherOne
+}
+
+function removeLastCat() {
+    const udone = [...cats.slice(0, -1)]
+    return udone
+}
+
+function removeFirstCat() {
+    const endit = [...cats.slice(1)]
+    return endit
+}
+
+
